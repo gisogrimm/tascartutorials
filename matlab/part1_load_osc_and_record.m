0e9558910b65a99df46560ffeba907d82a21ab8f
Darklab PC <g.grimm@uni-oldenburg.de>
2024-09-06 17:42:04 +0200

    improvement of matlab tutorial

--- matlab/part1_load_osc_and_record.m
+++ matlab/part1_load_osc_and_record.m
@@ -20,12 +20,12 @@ pause(3);
 
 %% Part 1.2: Modify parameters of the opened scene
 
-% Here is how to modify the TASCAR scene from MATLAB.  The scene
+% Here is how to modify the TASCAR scene from MATLAB. The scene
 % parameters can be modified from MATLAB using the 'send_osc.m'
 % function. You can set a breakpoint after the line where the scene is
 % loaded, and when the script stops at the breakpoint, go to VIEW->OSC
 % Variables in the TASCAR window - you will see a list of parameters
-% that can be changed.  You can experiment by setting breakpoints
+% that can be changed. You can experiment by setting breakpoints
 % after each use of 'send_osc' and each time see what has changed in
 % the scene or what action has been taken.
 
@@ -36,39 +36,39 @@ pos_z = 1;
 euler_x = 10;
 euler_y = 30;
 euler_z = 0;
-send_osc(h_temp,'/scene/src/pos' ,pos_x, pos_y, pos_z, euler_z, euler_y,euler_x)
+send_osc(h_temp, '/scene/src/pos', pos_x, pos_y, pos_z, euler_z, euler_y, euler_x)
 
 %% start playing back the scene:
 send_osc(h_temp, '/transport/start')
 
 %% Change the gain of an object:
-G=10;
-send_osc(h_temp,'/scene/src/0/gain' ,G)
+G = 10;
+send_osc(h_temp, '/scene/src/0/gain', G)
 
 %% stop playing back the scene:
-send_osc(h_temp,'/transport/stop')
+send_osc(h_temp, '/transport/stop')
 
 %% Change the position on the time line:
 time_point = 0.7;
-send_osc(h_temp,'/transport/locate',time_point);
+send_osc(h_temp, '/transport/locate', time_point)
 
 %% Part 1.3: Play & record
 
 % The following shows how to play and record TASCAR scene content
-% using the `tascar_jackio' tool.  It is necessary to specify the jack
+% using the `tascar_jackio' tool. It is necessary to specify the jack
 % audio ports corresponding to the sources and receivers. You can
 % again set a breakpoint after the line where the scene is loaded and
 % when the script stops at the breakpoint, go to the QjackCtl, click
 % the 'Connect' button and look at the jack port types and their
-% names.  You can also type 'jack_lsp -p' in the terminal to see the
+% names. You can also type 'jack_lsp -p' in the terminal to see the
 % list of available jack ports.
 
 % Specify jack ports:
 
 % create a cell string array with the names of writable clients (e.g. loudspeakers):
-c_Write={'render.scene:src.0.0'};
+c_Write = {'render.scene:src.0.0'};
 % create a cell string array with the names of readable clients (e.g. microphones): 
-c_Read={'render.scene:outomni.0'};
+c_Read = {'render.scene:outomni.0'};
 
 % set stimulus duration:
 duration = 3;
@@ -80,38 +80,41 @@ duration = 3;
 % help tascar_jackio
 
 % get the sample rate and buffer settings of your audio system:
-[tmp,fs,bufsize] = tascar_jackio(0);
+[tmp, fs, bufsize] = tascar_jackio(0);
 
 % To play back and record the scene with only its original content
-% (from the scene definition file), the test signal must only contain
-% zeros:
+% (from the scene definition file), the test signal must only contain zeros:
 
 % Create a test signal, with a column for each entry in the variable c_Write:
-test_sig = zeros(round(duration*fs),1);
+test_sig = zeros(round(duration*fs), 1);
 
 transportStart = 5; 
-[rec_signal_orig,fs,bufsize,load,xruns,sCfg] = tascar_jackio(test_sig, ...
-    'output' ,c_Write , 'input',  c_Read, 'starttime', transportStart);
+[rec_signal_orig, fs, bufsize, load, xruns, sCfg] = tascar_jackio(test_sig, ...
+    'output', c_Write , 'input', c_Read, 'starttime', transportStart);
+
+% plot the recorded signal
 figure
 plot(rec_signal_orig);
 
 % To play a sound created in Matlab and record it in the scene, the
-% stimulus can be created as a column vector, with one column per
-% entry in the variable c_Write.
+% stimulus can be created as a matrix, with one column per
+% entry in the variable c_Write and one row per audio sample.
 
 % create a Gaussian noise test signal:
-test_sig = 0.03*randn(round(duration*fs),1);
+test_sig = 0.03*randn(round(duration*fs), 1);
 
-transportStart=5; 
-[rec_signal_withnoise,fs,bufsize,load,xruns,sCfg]=tascar_jackio(test_sig, ...
-    'output' ,c_Write , 'input',  c_Read, 'starttime', transportStart);
+transportStart = 5; 
+[rec_signal_withnoise, fs, bufsize, load, xruns, sCfg]=tascar_jackio(test_sig, ...
+    'output', c_Write , 'input', c_Read, 'starttime', transportStart);
+
+% plot the recorded signal with whitenoise
 hold on
 plot(rec_signal_withnoise)
 
 % reverse order of plots to make the softer one visible:
-h = get(gca,'Children');
-set(gca,'Children',h(end:-1:1));
+h = get(gca, 'Children');
+set(gca, 'Children', h(end:-1:1));
 
 %% Part 1.4: Close a TASCAR scene:
 
-tascar_ctl('kill',h_temp);
+tascar_ctl('kill', h_temp);
--- matlab/part2_edit_xml_and_render_ir.m
+++ matlab/part2_edit_xml_and_render_ir.m
@@ -11,54 +11,77 @@ clear
 close all
 
 addpath /usr/share/tascar/matlab/
-javaaddpath ../netutil-1.0.0.jar
+
+% load the scene in tascar:
+part2 = tascar_ctl('load', 'part2.tsc');
+pause(3);
 
 %% --- Choose a TASCAR scene you would like to edit: ---
-doc = tascar_xml_open('/home/medi/Projects/tascarworkshop/matlab/task2_basic2.tsc');
+doc = tascar_xml_open('part2.tsc');
 
 %%  Change attributes of elements 
-r_walls=0.8;
-d_walls=0.2;
-ismorder = 2;
+r_walls = 0.8;
+d_walls = 0.2;
+ismorder = 1;
 
-%Change mirror order of scene:
-doc = tascar_xml_edit_elements( doc, 'scene', 'mirrororder', ismorder);
+% Change mirror order of scene:
+doc = tascar_xml_edit_elements(doc, 'scene', 'ismorder', ismorder);
 
-%Change r&d filter coefficients of the walls
-doc = tascar_xml_edit_elements( doc, 'facegroup', 'reflectivity', r_walls,'name','magicchamber');
-doc = tascar_xml_edit_elements( doc, 'facegroup', 'damping', d_walls,'name','magicchamber');
+% Change r&d filter coefficients of the walls
+doc = tascar_xml_edit_elements(doc, 'facegroup', 'reflectivity', r_walls, 'name', 'magicchamber');
+doc = tascar_xml_edit_elements(doc, 'facegroup', 'damping', d_walls, 'name', 'magicchamber');
 
-%Change size of the shoebox room 
-shoebox_size='3 5 3';
-doc = tascar_xml_edit_elements( doc, 'facegroup', 'shoebox', shoebox_size ,'name','walls');
+% Change size of the shoebox room 
+shoebox_size = '3 5 3';
+doc = tascar_xml_edit_elements(doc, 'facegroup', 'shoebox', shoebox_size , 'name', 'walls');
 
 %% --- Add new elements to  xml file --- 
 
-%Add a new source with a certain position and spherical interpolation
-sceneNode = tascar_xml_get_element( doc,'scene') ;
-srcNode = tascar_xml_add_element( doc, sceneNode{1}, 'source', [], 'name','new_source'); 
-posNode = tascar_xml_add_element( doc, srcNode, 'position','0 2 0 1 4 5 2 2','interpolation','spherical'); 
-soundNode = tascar_xml_add_element( doc, srcNode, 'sound', [], 'name','0');
-pluginsNode = tascar_xml_add_element( doc, soundNode, 'plugins', []);
-sndfileNode = tascar_xml_add_element( doc, pluginsNode,'sndfile',[],'name','${HOME}/tascar_scenes/sounds/daps/m4_script4_clean.wav','loop','0','level','60');
+% Add a new source with a certain position and spherical interpolation
+sceneNode = tascar_xml_get_element(doc, 'scene') ;
+srcNode = tascar_xml_add_element(doc, sceneNode{1}, 'source', [], 'name', 'new_source'); 
+posNode = tascar_xml_add_element(doc, srcNode, 'position', '0 2 0 1 4 5 2 2', 'interpolation', 'spherical'); 
+soundNode = tascar_xml_add_element(doc, srcNode, 'sound', [], 'name', '0');
+pluginsNode = tascar_xml_add_element(doc, soundNode, 'plugins', []);
+sndfileNode = tascar_xml_add_element(doc, pluginsNode, 'sndfile', [], 'resample', 'true', 'name', '${HOME}/tascartutorials/sounds/194463__agaxly__eating-chips.wav', 'loop', '0', 'level', '60');
 
 %% --- Removing elements from xml file ---
-sceneNode = tascar_xml_get_element( doc,'scene') ;
-positionnNode = tascar_xml_rm_element( doc, sceneNode{1}, 'obstacle', 'name','thewall'); 
+% bug to be fixed in tascar_xml_*
+% sceneNode = tascar_xml_get_element( doc,'scene') ;
+% positionnNode = tascar_xml_rm_element( doc, sceneNode{1}, 'obstacle', 'name','thewall'); 
 
 %% --- Save the edited scene definition file ---
-tascar_xml_save( doc, 'task2_basic2_modified.tsc' ) 
+tascar_xml_save(doc, 'part2_modified.tsc') 
 
+
+
+%% --- ONLINE RENDERING ---
+
+% Impulse response rendering (does require input signal, static) 
+
+% render the impulse response of the unmodified scene 
+[rendered_IR1, fs1] = tascar_ir_measure('input', 'render.scene:out_omni.0', 'output', 'render.scene:src.0.0');
+% close the unmodified scene
+tascar_ctl('kill', part2);
+pause(3);
+
+% open the modified scene in Tascar
+part2_mod = tascar_ctl('load', 'part2_modified.tsc');
+pause(5);
+% render the impulse response of the modified scene 
+[rendered_IR2, fs2] = tascar_ir_measure('input', 'render.scene:out_omni.0', 'output', 'render.scene:src.0.0');
+% close the modified scene
+tascar_ctl('kill', part2_mod);
+pause(3);
 
 
 %% --- OFFLINE RENDERING ---
 
-%% Impulse response rendering (does not require input signal, static) 
+% Impulse response rendering (does not require input signal, static) 
 
-% open the unmodified scene in Tascar
-[rendered_IR1,fs1] = tascar_ir_measure('input','render.scene:out_omni','output','render.scene:src.0');
+% render the impulse response of the unmodified scene 
+system('LD_LIBRARY_PATH='''' tascar_renderir -o ir_unmodified.wav part2.tsc')
 
-% open the modified scene in Tascar
-[rendered_IR2,fs2] = tascar_ir_measure('input','render.scene:out_omni','output','render.scene:src.0');
-
+% render the impulse response of the modified scene 
+system('LD_LIBRARY_PATH='''' tascar_renderir -o ir_modified.wav part2_modified.tsc')
 
